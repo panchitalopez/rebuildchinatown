554c9a85bf0bd24a7a38feba7f1b83d1b255372c
Panchita Lopez-Li <62720766+panchitalopez@users.noreply.github.com>
2021-12-13 12:35:38 -0500

    Update covidclosures.py

--- covidclosures.py
+++ covidclosures.py
@@ -1,8 +1,15 @@
 #creating smaller CSV file because my original CSV file had a lot of columns that were unncecessary
-
 inputfile = input('Enter inputfile name: ')
 outputfile = input('Enter outputfile name: ')
 df = pd.read_csv(inputfile)
 new = df[['Name','Cuisine', 'Type', 'Borough', 'ZIP', 'Lat','Long']]
 new.to_csv(outputfile)
 
+#Analysis 1: to find count and percentage of cuisine (General). 
+outputfile = input('Enter outputfile name: ')
+df = pd.read_csv('ult.csv') 
+x = df.Cuisine_General.value_counts(dropna=False)
+y = df.Cuisine_General.value_counts(dropna=False, normalize=True)
+z = pd.concat([x,y], axis=1, keys= ['counts', '%'])
+print (z) 
+    
